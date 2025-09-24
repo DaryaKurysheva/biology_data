3156280682b0cb06c6412bf57feac5d7289895d9
DaryaKurysheva <kurysevadara57@gmail.com>
2025-09-24 16:32:22 +0300

    добавлено

--- data_loader.py
+++ data_loader.py
@@ -2,7 +2,70 @@ import pandas as pd
 
 file_name = "data/Evolution_DataSets.csv"
 
-raw_data = pd.read_csv(file_name)
+pd = raw_data.read_csv(file_name)
 
 print(raw_data.head(10))
 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+import pandas as pd
+
+# In[ ]:
+
+evl=pd.read_csv('data/Evolution_DataSets.csv')
+evl
+
+# In[ ]:
+
+evl.info()
+
+# In[ ]:
+
+evl.dtypes
+
+# In[ ]:
+
+evl.Location=evl['Location'].astype('category')
+
+# In[ ]:
+
+evl.dtypes
+
+# In[ ]:
+
+evl.describe()
+
+# In[ ]:
+
+evl.Migrated=evl['Migrated'].astype('object')
+
+# In[ ]:
+
+evl.dtypes
+
+# In[ ]:
+
+fields=['Cranial_Capacity','Height']
+
+# In[ ]:
+
+evl[fields].corr
+
+# In[ ]:
+
+pip install parquet
+
+# In[ ]:
+
+pip install pyarrow
+
+# In[ ]:
+
+pip install fastparquet
+
+# In[ ]:
+
+evl.to_parquet('data/Evolution_DataSets.parquet')
